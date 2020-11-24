7829740b1e7e8fbc67a2c837068c7c2eea1a768a
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-11-24 17:34:25 +0000

    giftcard class set up and tested. passing ok

--- src/main/java/GiftCard.java
+++ src/main/java/GiftCard.java
@@ -1,2 +1,21 @@
 public class GiftCard {
+
+    private String retailer;
+    private Double balance;
+
+    public GiftCard(String retailer, Double balance){
+        this.retailer = retailer;
+        this.balance = balance;
+    }
+
+    public String getRetailer(){
+        return this.retailer;
+    }
+     public double getBalance(){
+        return this.balance;
+     }
+
+     public void chargeCustomer(Double chargedAmount){
+        this.balance -= chargedAmount;
+     }
 }
--- src/test/java/GiftCardTest.java
+++ src/test/java/GiftCardTest.java
@@ -1,2 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GiftCardTest {
+
+    private GiftCard giftCard;
+
+    @Before
+    public void setUp(){
+        giftCard = new GiftCard("Howies", 100.00);
+    }
+
+    @Test
+    public void canGetRetailer(){
+        assertEquals("Howies", giftCard.getRetailer());
+    }
+
+    @Test
+    public void canGetBalance(){
+        assertEquals(100.00, giftCard.getBalance(), 0.01);
+    }
+
+    @Test
+    public void canChargeCustomer(){
+        giftCard.chargeCustomer(20.00);
+        assertEquals(80.00, giftCard.getBalance(), 0.01);
+    }
+
+
+
+
+
 }
